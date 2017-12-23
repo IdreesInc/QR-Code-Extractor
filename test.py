de01fa2ca161d14fc7b4b65161c662d7d7d6ba61
Idrees Hassan <idrees@idreesinc.com>
2017-12-23 16:47:10 -0500

    Added ability to extract QR codes from video streams.

---
+++ test.py
@@ -0,0 +1,18 @@
+import cv2
+
+import qr_extractor as reader
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    codes, frame = reader.read(frame, True)
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print "I quit!"
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
